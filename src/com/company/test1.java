57912c38f688bc6948f7248ca1f936a0ba581430
aibianchengdejjle <1044839685@qq.com>
2020-09-27 22:30:14 +0800

    add

--- src/com/company/test1.java
+++ src/com/company/test1.java
@@ -1,5 +1,27 @@
 package com.company;
 
-public class test1 {
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayListHandler;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class test1 {
+    public static void main(String[] args) throws SQLException {
+        BasicDataSource bds=creategongju.getbss();
+        Connection c=bds.getConnection();
+        String sql="select * from users";
+        QueryRunner qr=new QueryRunner();
+        List<Object[]>a=qr.query(c,sql,new ArrayListHandler());
+        for (Object []B:a
+             ) {
+            for (Object r:B
+                 ) {
+                System.out.print(r+"    ");
+            }
+            System.out.println();
+        }
+    }
 }
